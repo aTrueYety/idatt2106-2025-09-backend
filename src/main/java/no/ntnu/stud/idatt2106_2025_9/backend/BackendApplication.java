de31337330ed57c0438daf78ec22525e37fa4768
aTrueYety <erikhoff00@gmail.com>
2025-04-22 15:26:18 +0200

    init mvn

---
+++ src/main/java/no/ntnu/stud/idatt2106_2025_9/backend/BackendApplication.java
@@ -0,0 +1,13 @@
+package no.ntnu.stud.idatt2106_2025_9.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/no/ntnu/stud/idatt2106_2025_9/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package no.ntnu.stud.idatt2106_2025_9.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
