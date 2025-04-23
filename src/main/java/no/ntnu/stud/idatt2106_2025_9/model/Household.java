40d8a83eecefab55d6e4f6d15d0cfa498f359312
Henrik André Gaustd <hengau@live.no>
2025-04-23 11:42:50 +0200

    Add javadoc and lombok to Household class

---
+++ src/main/java/no/ntnu/stud/idatt2106_2025_9/model/Household.java
@@ -0,0 +1,25 @@
+package no.ntnu.stud.idatt2106_2025_9.model;
+
+import java.sql.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Entity representing a HouseHold.
+ * 
+ * @version 1.0
+ * @since 23.04.2025
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Household {
+    private Long id;
+    private String adress;
+    private double longditude;
+    private double latitude;
+    private double waterAmountLiters;
+    private Date lastWaterChange;
+}
