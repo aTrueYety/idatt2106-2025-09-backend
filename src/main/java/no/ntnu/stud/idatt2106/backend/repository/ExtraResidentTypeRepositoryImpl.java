00f6903f858365d2ab8bf081da6b66c3154ae926
Henrik sund <henriksund@dhcp-10-22-170-51.wlan.ntnu.no>
2025-04-24 15:38:17 +0200

    added resident repository

---
+++ src/main/java/no/ntnu/stud/idatt2106/backend/repository/ExtraResidentTypeRepositoryImpl.java
@@ -0,0 +1,57 @@
+package no.ntnu.stud.idatt2106.backend.repository;
+
+import no.ntnu.stud.idatt2106.backend.model.base.ExtraResidentType;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class ExtraResidentTypeRepositoryImpl implements ExtraResidentTypeRepository {
+
+  private final JdbcTemplate jdbc;
+
+  public ExtraResidentTypeRepositoryImpl(JdbcTemplate jdbc) {
+    this.jdbc = jdbc;
+  }
+
+  private final RowMapper<ExtraResidentType> rowMapper = (rs, rowNum) -> {
+    ExtraResidentType type = new ExtraResidentType();
+    type.setId(rs.getInt("id"));
+    type.setName(rs.getString("name"));
+    type.setConsumptionWater(rs.getFloat("consumption_water"));
+    type.setConsumptionFood(rs.getFloat("consumption_food"));
+    return type;
+  };
+
+  @Override
+  public Optional<ExtraResidentType> findById(int id) {
+    String sql = "SELECT * FROM extra_resident_type WHERE id = ?";
+    return jdbc.query(sql, rowMapper, id).stream().findFirst();
+  }
+
+  @Override
+  public List<ExtraResidentType> findAll() {
+    String sql = "SELECT * FROM extra_resident_type";
+    return jdbc.query(sql, rowMapper);
+  }
+
+  @Override
+  public void save(ExtraResidentType type) {
+    String sql = "INSERT INTO extra_resident_type (name, consumption_water, consumption_food) VALUES (?, ?, ?)";
+    jdbc.update(sql, type.getName(), type.getConsumptionWater(), type.getConsumptionFood());
+  }
+
+  @Override
+  public void update(ExtraResidentType type) {
+    String sql = "UPDATE extra_resident_type SET name = ?, consumption_water = ?, consumption_food = ? WHERE id = ?";
+    jdbc.update(sql, type.getName(), type.getConsumptionWater(), type.getConsumptionFood(), type.getId());
+  }
+
+  @Override
+  public void deleteById(int id) {
+    jdbc.update("DELETE FROM extra_resident_type WHERE id = ?", id);
+  }
+}
