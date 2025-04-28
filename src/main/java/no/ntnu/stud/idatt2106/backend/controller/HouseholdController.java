9769da7669b5f3f54e7c9ab320dcddeca57f7578
Henrik André Gaustd <hengau@live.no>
2025-04-28 10:09:55 +0200

    Add endpoint for getting users from household

--- src/main/java/no/ntnu/stud/idatt2106/backend/controller/HouseholdController.java
+++ src/main/java/no/ntnu/stud/idatt2106/backend/controller/HouseholdController.java
@@ -6,7 +6,9 @@ import java.util.List;
 import no.ntnu.stud.idatt2106.backend.model.request.AddUserHouseholdRequest;
 import no.ntnu.stud.idatt2106.backend.model.request.HouseholdRequest;
 import no.ntnu.stud.idatt2106.backend.model.response.HouseholdResponse;
+import no.ntnu.stud.idatt2106.backend.model.response.UserResponse;
 import no.ntnu.stud.idatt2106.backend.service.HouseholdService;
+import no.ntnu.stud.idatt2106.backend.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,30 +21,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- *  Controller class responsible for operations related to Households,
- *  like registering a new household or adding users to an existing household.
+ * Controller class responsible for operations related to Households,
+ * like registering a new household or adding users to an existing household.
  */
-@Tag(
-    name = "Households",
-    description = "Endpoints for household operations"
-)
+@Tag(name = "Households", description = "Endpoints for household operations")
 @RestController
 @RequestMapping("api/households")
 public class HouseholdController {
   private static final Logger logger = LoggerFactory.getLogger(HouseholdController.class);
-  
+
   @Autowired
   private HouseholdService householdService;
+
+  @Autowired
+  private UserService userService;
 
   /**
    * Returns all of the registered households as HouseholdResponses.
    *
    * @return the registered households with a status code
    */
-  @Operation(
-      summary = "Retrieve all registered households",
-      description = "Retrieves information about all of the registered households"
-  )
+  @Operation(summary = "Retrieve all registered households", description = "Retrieves information about all of the registered households")
   @GetMapping
   public ResponseEntity<List<HouseholdResponse>> getAll() {
     List<HouseholdResponse> households = householdService.getAll();
@@ -54,12 +53,10 @@ public class HouseholdController {
    * Handles request to register a new household.
    *
    * @param householdRequest the request containing the details of the household
-   * @return a ResponseEntity containing the registration response or an error message.
+   * @return a ResponseEntity containing the registration response or an error
+   *         message.
    */
-  @Operation(
-      summary = "Creates a new household",
-      description = "Creates a new household with the user creating it as a member"
-  )
+  @Operation(summary = "Creates a new household", description = "Creates a new household with the user creating it as a member")
   @PostMapping("/register")
   public ResponseEntity<?> registerHousehold(@RequestBody HouseholdRequest householdRequest) {
     householdService.registerHousehold(householdRequest);
@@ -70,14 +67,13 @@ public class HouseholdController {
   /**
    * Handles request to add a user to a household.
    *
-   * @param request object with the id of the household and the username of the user to be added
-   * @return a ResponseEntity with the response to the operation or an error message
+   * @param request object with the id of the household and the username of the
+   *                user to be added
+   * @return a ResponseEntity with the response to the operation or an error
+   *         message
    */
-  @Operation(
-      summary = "Adds a user to a household",
-      description = "Adds the user with the specified username to the "
-      + "household with the specified ID"
-  )
+  @Operation(summary = "Adds a user to a household", description = "Adds the user with the specified username to the "
+      + "household with the specified ID")
   @PostMapping("add-user")
   public ResponseEntity<?> addUserToHousehold(@RequestBody AddUserHouseholdRequest request) {
     householdService.addUserToHousehold(request);
@@ -91,17 +87,32 @@ public class HouseholdController {
    * @param id the ID off the household to be retrieved
    * @return a ResponseEntity with the retrieved Household
    */
-  @Operation(
-      summary = "Retrieves the household with the given ID",
-      description = """
-          Retrieves information about the household with the specified ID
-          if it exists.
-          """
-  )
+  @Operation(summary = "Retrieves the household with the given ID", description = """
+      Retrieves information about the household with the specified ID
+      if it exists.
+      """)
   @GetMapping("/{id}")
   public ResponseEntity<HouseholdResponse> getById(@PathVariable Long id) {
     HouseholdResponse response = householdService.getById(id);
     logger.info("Retrieved household successfully");
     return ResponseEntity.ok().body(response);
+  }
+
+  /**
+   * Handles request to get all of the users in a Household.
+   *
+   * @param householdId the ID of the household to retrieve users from
+   * @return a ResponseEntity with the retrieved users
+   */
+  @Operation(summary = "Retrieves all users from the household with the given ID", description = """
+      Retrieves public user information like username, email, name, and profile picture
+      of all of the users in the household with the specified ID.
+      """)
+  @GetMapping("/{householdId}/users")
+  public ResponseEntity<List<UserResponse>> getHouseholdMembers(@PathVariable Long householdId) {
+    logger.info("Fetching user members for household with ID: {}", householdId);
+    List<UserResponse> response = userService.getUsersByHouseholdId(householdId);
+    logger.info("Found {} members for household ID: {}", response.size(), householdId);
+    return ResponseEntity.ok(response);
   }
 }
--- src/main/java/no/ntnu/stud/idatt2106/backend/service/UserService.java
+++ src/main/java/no/ntnu/stud/idatt2106/backend/service/UserService.java
@@ -1,7 +1,12 @@
 package no.ntnu.stud.idatt2106.backend.service;
 
+import no.ntnu.stud.idatt2106.backend.mapper.UserMapper;
 import no.ntnu.stud.idatt2106.backend.model.base.User;
+import no.ntnu.stud.idatt2106.backend.model.response.UserResponse;
 import no.ntnu.stud.idatt2106.backend.repository.UserRepository;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -49,5 +54,18 @@ public class UserService {
    */
   public void updateUserCredentials(User user) {
     userRepo.updateUser(user);
+  }
+
+  /**
+   * Retrieves all users belonging to the household with the specified ID,
+   * and maps them to UserResponses.
+   *
+   * @param householdId the ID of the household to retrieve Users from
+   * @return a List of {@Link UserResponse} representing the users in the given household
+   */
+  public List<UserResponse> getUsersByHouseholdId(Long householdId) {
+    return userRepo.findUsersByHouseholdId(householdId).stream()
+      .map(UserMapper::toResponse)
+      .toList();
   }
 }
