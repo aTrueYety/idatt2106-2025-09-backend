87b586116d51fbb5360de846153dce2f534dbf39
aTrueYety <erikhoff00@gmail.com>
2025-04-29 12:02:57 +0200

    Add InfoPageMapper class for converting between InfoPage and DTO objects

---
+++ src/main/java/no/ntnu/stud/idatt2106/backend/mapper/InfoPageMapper.java
@@ -0,0 +1,42 @@
+package no.ntnu.stud.idatt2106.backend.mapper;
+
+import no.ntnu.stud.idatt2106.backend.model.base.InfoPage;
+import no.ntnu.stud.idatt2106.backend.model.request.InfoPageRequest;
+import no.ntnu.stud.idatt2106.backend.model.update.InfoPageUpdate;
+
+/**
+ * Mapper class for converting between InfoPage and InfoPageDTO objects.
+ */
+public class InfoPageMapper {
+  /**
+   * Converts an InfoPageRequest object to an InfoPage object.
+   *
+   * @param infoPageRequest the InfoPageRequest object to convert
+   * @return the converted InfoPage object
+   */
+  public static InfoPage requestToInfoPage(InfoPageRequest infoPageRequest) {
+    return new InfoPage(
+      null, 
+      infoPageRequest.getTitle(), 
+      infoPageRequest.getContent(),
+      null,
+      null
+    );
+  }
+
+  /**
+   * Converts an InfoPageUpdate object to an InfoPage object.
+   *
+   * @param infoPage the InfoPage object to convert
+   * @return the converted InfoPageUpdate object
+   */
+  public static InfoPage infoPageToUpdate(InfoPageUpdate infoPage) {
+    return new InfoPage(
+      infoPage.getId(), 
+      infoPage.getTitle(), 
+      infoPage.getContent(),
+      null, 
+      null
+    );
+  }
+}
