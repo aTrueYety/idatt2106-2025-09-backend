334f09c658accbe7f434597d8b2e17ec82499a85
Henrik André Gaustd <hengau@live.no>
2025-04-30 15:17:51 +0200

    Fix typo in household response should fix issue

--- src/main/java/no/ntnu/stud/idatt2106/backend/model/request/HouseholdRequest.java
+++ src/main/java/no/ntnu/stud/idatt2106/backend/model/request/HouseholdRequest.java
@@ -12,7 +12,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class HouseholdRequest {
-  private String adress;
+  private String address;
   private Double longitude;
   private Double latitude;
   private Double waterAmountLiters;
