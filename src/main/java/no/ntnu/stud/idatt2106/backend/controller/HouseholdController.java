537ecba6bdb3a857ca3e9b192f5354f2e40b7adf
Henrik André Gaustd <hengau@live.no>
2025-04-30 12:00:32 +0200

    Fix endpoint for getting users household

--- src/main/java/no/ntnu/stud/idatt2106/backend/controller/HouseholdController.java
+++ src/main/java/no/ntnu/stud/idatt2106/backend/controller/HouseholdController.java
@@ -186,7 +186,7 @@ public class HouseholdController {
   public ResponseEntity<HouseholdResponse> getCurrentUserHousehold(
       @RequestHeader("Authorization") String token) {
     logger.info("Fetching household of authenticated user");
-    Long userId = jwtService.extractUserId(token);
+    Long userId = jwtService.extractUserId(token.substring(7));
     HouseholdResponse response = householdService.getByUserId(userId);
     logger.info("Found household of user with ID: {}", userId);
     return ResponseEntity.ok(response);
