adf3c4991be3063994aeed466f984d67500c2754
aTrueYety <erikhoff00@gmail.com>
2025-05-06 14:35:05 +0200

    Refactor Household creation logic to use HouseholdMapper for improved code clarity and maintainability

--- src/main/java/no/ntnu/stud/idatt2106/backend/service/HouseholdService.java
+++ src/main/java/no/ntnu/stud/idatt2106/backend/service/HouseholdService.java
@@ -129,13 +129,7 @@ public class HouseholdService {
     Validate.that(householdReqeust.getLatitude(),
         Validate.isNotNull(), "Latitude cannot be null");
 
-    Household household = new Household();
-    household.setName(householdReqeust.getName());
-    household.setAddress(householdReqeust.getAddress());
-    household.setLatitude(householdReqeust.getLatitude());
-    household.setLongitude(householdReqeust.getLongitude());
-    household.setWaterAmountLiters(householdReqeust.getWaterAmountLiters());
-    household.setLastWaterChangeDate(householdReqeust.getLastWaterChangeDate());
+    Household household = HouseholdMapper.toEntity(householdReqeust);
 
     Household registeredHousehold = householdRepository.save(household);
 
