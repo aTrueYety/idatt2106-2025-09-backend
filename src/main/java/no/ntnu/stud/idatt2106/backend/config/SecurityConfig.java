9e09dc1f4c6aa54343f55c83585b56d95f6a69ba
aTrueYety <erikhoff00@gmail.com>
2025-05-08 22:03:28 +0200

    Remove unnecessary blank line in SecurityConfig.java

--- src/main/java/no/ntnu/stud/idatt2106/backend/config/SecurityConfig.java
+++ src/main/java/no/ntnu/stud/idatt2106/backend/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package no.ntnu.stud.idatt2106.backend.config;
 
 import java.util.Arrays;
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
